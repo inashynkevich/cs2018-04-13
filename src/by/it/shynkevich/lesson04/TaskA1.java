70a1a6d86cf67c0b1e8b5898248d5d4f0022d06a
inashynkevich <ina.shynkevich@gmail.com>
2018-04-24 23:56:32 +0300

    lesson4 tasks A1-A3 copmleted

--- src/by/it/shynkevich/lesson04/TaskA1.java
+++ src/by/it/shynkevich/lesson04/TaskA1.java
@@ -26,13 +26,11 @@ package by.it.shynkevich.lesson04;
  */
 public class TaskA1 {
     public static void main(String[] args) {
-        for (int i = 1; 1 <=10; i++){
-            if (i != 3 && i != 9){
-                System.out.println("Я изучаю программирование.");
-        } else{
-            System.out.println("Меня интересует высокая зарплата!");
-        }
-
+        for (int i = 1; i <=10; i++){
+            if (i == 3 || i == 9){
+                System.out.println("Меня интересует высокая зарплата!");
+        } else
+            System.out.println("Я изучаю программирование.");
 
         }
     }
